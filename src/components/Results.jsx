ea402cf83bdf009517c9c9036bd6410b03aa3b3f
Marta Paz <pazmarta@gmail.com>
2025-06-12 17:32:45 +0100

    render table results
    - outputting results in a List and deriving more values

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,12 +2,40 @@
 // It will get the user input as an input here instead of the UserInput component,
 // so that the results can be calculated and displayed dynamically.
 // and derive the results from the input prop here (= user input state) -> computing values based on state
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ input }) {
     const resultsData = calculateInvestmentResults(input); // it will be an of object *see util/investment.js line 7
-    console.log(resultsData); // for debugging purposes
+    // console.log(resultsData);
+    const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
+
     return (
-        <p>Results ... </p>        
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map((yearData) => {
+                   const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
+                   const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+                   return (
+                    <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totalInterest)}</td>
+                        <td>{formatter.format(totalAmountInvested)}</td>
+                    </tr>
+                   );
+                })}
+            </tbody>
+            </table>
     );
 }
